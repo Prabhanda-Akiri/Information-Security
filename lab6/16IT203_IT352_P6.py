328d93ae4b13c5ea41e4fd0643bb396b229dea3c
Akiri Prabhanda <31247199+Prabhanda-Akiri@users.noreply.github.com>
2019-03-20 02:12:08 +0530

    Update 16IT203_IT352_P6.py

--- lab6/16IT203_IT352_P6.py
+++ lab6/16IT203_IT352_P6.py
@@ -1,11 +1,12 @@
 import sys
 import fractions
+import math
 
 def main():
 
-	plain_text=input('\nEnter the plain text:\n')
+	plain_text=input('\nEnter the Message:\n')
 	P=int(input('\nP:	'))
-	Q=int(input('\nQ:	'))
+	Q=int(input('Q:	'))
 	N=P*Q
 	#phi_N=calculate_phi_N(N)
 	phi_N=(P-1)*(Q-1)
@@ -13,25 +14,73 @@ def main():
 	e,d=get_multiplicative_inverse(phi_N,'e')
 	r,r_inv=get_multiplicative_inverse(N,'r')
 
-
+	print('N=',N)
 	plain_text_blocks=block_formation(plain_text,N)
-	print('blocks',plain_text_blocks)
+	#print('\nOriginal Message:		(M)',plain_text_blocks)
 	wrapped_blocks=wrap_messages(plain_text_blocks,r,e,N)
-	print('message*r_power_e:\n',wrapped_blocks)
+	#print('\nMessage wrapped by attacker:		(M_dash=M*r^e mod N)\n',wrapped_blocks)
 	signed_blocks=sign_messages(wrapped_blocks,d,N)
-	print('signed_blocks: m_power_d:\n',signed_blocks)
+	#print('\nSigned message from Bob:		(S=M^d *r mod N)\n',signed_blocks)
 	signatures=get_signatures(signed_blocks,r_inv,N)
-	print('signatures:\n',signatures)
+	#print('\nSignature of Bob:		(S_dash=M^d mod N)\n',signatures)
 
+	recovered_message=recover_message(signatures,e,N)
+	# print('\nMessage derived from Bob\'s Signature:\n',recovered_message)
 	calculated_signatures=calculate_signatures(plain_text_blocks,d,N)
-	print('calculated_signatures:\n',calculated_signatures)
+	#print('\ncalculated_signatures for verification:		(M^d mod N)\n',calculated_signatures)
+	display_results(plain_text_blocks,wrapped_blocks,signed_blocks,recovered_message,signatures,calculated_signatures,plain_text)
 
-def calculate_signatures(plain_text_blocks,d,N):
+def display_results(plain_text_blocks,wrapped_blocks,signed_blocks,recovered_message,signatures,calculated_signatures,plain_text):
+	
+	recovered_text=''.join(hex(e)[2:] for e in recovered_message)
+	recovered_text=bytearray.fromhex(recovered_text).decode()
+
+
+	for i in range(len(plain_text_blocks)):
+		print('\nBlock:	',i+1)
+		print('Original Message(M):		',plain_text_blocks[i])
+		print('Message wrapped by attacker(M_dash=M*r^e mod N):		',wrapped_blocks[i])
+		print('Signed message from Bob(S=M^d *r mod N):			',signed_blocks[i])
+		print('Signature of Bob(S_dash=M^d mod N):				',signatures[i])
+		print('calculated_signatures for verification(M^d mod N):		',calculated_signatures[i])
+
+	if signatures==calculated_signatures:
+		print('\n\nBob\'s signature  and calculated Bob\'s Signature are matching..!\nBlinding attack is Successful\n')
+
+	print('\nOriginal Message:		(M)\n',plain_text)
+	print('\nOriginal Message derived from Bob\'s Signature:		(M= S_dash^e mod N)\n',recovered_text)
+	#signature_text=''.join(bytearray.fromhex(hex(e)[2:]).decode() for e in signatures)
+	#print(signature_text)
+
+	# signature_chars=[]
+	# for block in signatures:
+	# 	c=str(block)
+	# 	ch=''
+	# 	if len(c)%2!=0:
+	# 		c='0'+c 
+
+	# 	for i in range(0,len(c),2):
+	# 		if i+1<len(c):
+	# 			ch+=bytes.fromhex(hex(int(c[i]+c[i+1]))[2:]).decode('utf-8')
+	# 		else:
+	# 			ch+=bytes.fromhex(hex(int(c[i]))[2:]).decode('utf-8')
+
+	# 	signature_chars.append(ch)
+	
+	# print('\nBob\'s Signature:\n',''.join(signature_chars))
+	
+
+	
+def split(str, num):
+    return [ str[start:start+num] for start in range(0, len(str), num) ]
+
+def recover_message(signatures,e,N):
 	calculated_signatures=[]
-	for block in plain_text_blocks:
-		calculated_signatures.append(get_pow_mod(block,d,N))
+	for block in signatures:
+		calculated_signatures.append(get_pow_mod(block,e,N))
 
 	return calculated_signatures
+
 def get_multiplicative_inverse(N,s):
 	c=0
 	while True:
@@ -41,7 +90,8 @@ def get_multiplicative_inverse(N,s):
 		d=calculate_inv_modulo(e,N)
 		if c<3:
 			if d==None:
-				print('inverse modulo can\'t be calculated for given',s,', enter another value')
+				print('\nNo inverse modulo')
+				sys.exit()
 				continue
 			else:
 				break
@@ -52,12 +102,12 @@ def get_multiplicative_inverse(N,s):
 def calculate_phi_N(n):
 	count=0        
 	for i in range(1, n + 1):
-		if fractions.gcd(n, i) == 1:
+		if math.gcd(n, i) == 1:
 			count += 1
 	return count
 
 def calculate_inv_modulo(a,N):
-	if fractions.gcd(N,a)!=1:
+	if math.gcd(N,a)!=1:
 		return 
 	q=[]
 	r1=N
@@ -80,7 +130,7 @@ def calculate_inv_modulo(a,N):
 		return t1+N
 	return t1
 
-def wrap_messages(plain_text_blocks,N,e,r):
+def wrap_messages(plain_text_blocks,r,e,N):
 
 	wrapped_blocks=[]
 	for block in plain_text_blocks:
@@ -88,7 +138,7 @@ def wrap_messages(plain_text_blocks,N,e,r):
 		t2=get_pow_mod(r,e,N)
 		temp=(t1*t2) % N   #(A * B) mod C = (A mod C * B mod C) mod C
 		wrapped_blocks.append(temp)
-
+		#print(t1,t2,temp)
 	return wrapped_blocks
 
 def sign_messages(wrapped_blocks,d,N):
@@ -116,18 +166,20 @@ def block_formation(plain_text,N):
 		plain_text_hex.append(hex(ord(character))[2:])
 	#print(plain_text_hex)
 
-	for iter_i in plain_text_hex:
-		#print(int(buff,16),int(iter_i,16))
-		#print(int(buff+iter_i,16))
-		if int(buff+iter_i,16) > N:
-			blocks.append(int(buff,16))
-			buff=iter_i
-		else:
-			buff+=iter_i
-	blocks.append(int(buff,16))
-	print(plain_text_hex)
+	powers=[8,16,24,32,40,48,56,64]
+	block_size=0
+
+	for i in range(len(powers)):
+		temp=2**powers[i]
+		if temp>N:
+			block_size=i 
+			break
+	
+	blocks=[plain_text_hex[i:i + block_size] for i in range(0, len(plain_text_hex), block_size)]
+	blocks=[int(''.join(i),16) for i in blocks]
 
 	return blocks
+
 
 def get_pow_mod(a,p,N):
 	
@@ -167,6 +219,13 @@ def convert_to_binary(x):
 	# 	val[i]=int(val[i])
 	#print(val)
 	return val
+
+def calculate_signatures(plain_text_blocks,d,N):
+	calculated_signatures=[]
+	for block in plain_text_blocks:
+		calculated_signatures.append(get_pow_mod(block,d,N))
+
+	return calculated_signatures
 
 if __name__ == '__main__':
 	main()
